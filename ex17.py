46acadba3d0e24593f7f3034b2dac622c2f6d1d7
Kim-Gimin <kimjjang1997@gmail.com>
2023-05-25 17:22:31 +0900

    빅데이터 연습

--- ex17.py
+++ ex17.py
@@ -6,5 +6,6 @@ f2 = np.random.normal(loc = 3, scale=5, size=10000)
 
 plt.hist(f1, bins=200, color='red', alpha=.7, label='loc = 0, scale = 1')
 plt.hist(f2, bins=200, color='blue', alpha=.5, label='loc = 3, scale = .5')
-fig.savefig('historgam.png')
+
 plt.show()
+fig.savefig('historgam.png')
